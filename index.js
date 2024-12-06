588f142fb853ee22f70fbe48e13ea599add683c8
aidadieudonne <aidaharing10@gmail.com>
2024-12-06 15:57:28 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+ customerName();
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    }
+
+
+var bestCustomer = 'not bob'
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    }
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'karen'
+    leastFavoriteCustomer = 'mona';
+    console.log(leastFavoriteCustomer);
+}
